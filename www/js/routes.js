39ac5e7e698b89e820d84696796fc4df02db2d35
Mojo Layers <mojolayers@proton.me>
2022-09-06 20:34:50 +1000

    Start Building Wild Flower (Public UI Module)

--- www/js/routes.js
+++ www/js/routes.js
@@ -5,11 +5,6 @@ var routes = [
     url: './index.html',
   },
   {
-    // Might delete this route as if it doesn't implement well.
-    path: '/about/',
-    url: './pages/about.html',
-  },
-  {
     path: '/paper-plane/',
     url: './pages/udid.html',
   },
@@ -43,6 +38,49 @@ var routes = [
       resolve(
         {
           componentUrl: './pages/udid-template.html',
+        },
+        {
+          props: {
+            deviceProfile: deviceProfile,
+          }
+        }
+      );
+    },
+  },
+  {
+    /*
+      Wild Flower is the internal name for UDID.Report's User Interface
+      This page exists to allow for debugging the User Interface.
+    */
+    path: '/wild-flower/',
+    async: function ({ router, to, resolve }) {
+      // Access app instance
+      var app = router.app;
+      
+      // Access Cookies
+      const cookie = Object.fromEntries(document.cookie.split('; ').map(c => c.split('=')));
+      var udidCookie = "";
+      var isEnrolled = false;
+
+      // Check if device is enrolled via cookies.
+      if (!("udid" in cookie)) {
+        // No Exisiting Enrollment Found.
+        udidCookie = 'No UDID Saved, please Enroll.';
+      } else {
+        udidCookie = cookie['udid'];
+        isEnrolled = true;
+      }
+
+      var deviceProfile = {
+        id: 0,
+        udid: udidCookie,
+        isEnrolled: isEnrolled
+      };
+
+      // Resolve route to load page
+      resolve(
+        {
+          componentUrl: './pages/wild-flower.html',
         },
         {
           props: {
